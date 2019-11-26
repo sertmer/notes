1d147a4cfa494bf3a9b39accd5dd3ed6cf11decb
sertmer <scottertmer@gmail.com>
2019-11-25 18:37:59 -0700

    Rebuild App

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  constructor() {
+    super()
+  }
+  
+  render() {
+    return (
+      <section className='app'>
+        <h1>hi</h1>
+      </section>
+    )
+  }
+}
+
+
+export default App
